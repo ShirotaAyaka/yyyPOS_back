6bfcbcb623c817d9f3cc81fc5b9902c21ac8fb44
ShirotaAyaka <yakka.as37@gmail.com>
2024-10-20 01:26:08 +0900

    ファイルを追加

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+
+app = FastAPI() #ベース。空箱を作る
+
+# .envファイルの内容をロード
+load_dotenv()
+
+# CORS設定 (NEXT.jsからのリクエストを許可する)
+origins = [
+    "http://localhost:3000",  # NEXT.jsのデフォルトポート
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message": "FastAPIで作成"}
+
+@app.get("/check")
+async def check():
+    return {"message": "確認しました"}
